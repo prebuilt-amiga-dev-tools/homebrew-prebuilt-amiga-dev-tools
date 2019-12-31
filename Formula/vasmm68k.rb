52529843b7ab727c2b0d18ae9a7808927e27a9b2
Mikael Kalms <mikael@kalms.org>
2019-12-31 16:38:52 +0100

    Fixed whitespace warnings from brew audit --strict

--- Formula/vasmm68k.rb
+++ Formula/vasmm68k.rb
@@ -1,21 +1,21 @@
 class Vasmm68k < Formula
-    desc "Cross-assembler for 680x0 processors / Amiga OS"
-    homepage "http://sun.hasenbraten.de/vasm"
-    url "http://server.owl.de/~frank/tags/vasm1_8e.tar.gz"
-    version "1.8e"
-    sha256 "abcd1234"
-  
-    def install
-      # ENV.deparallelize
-      system "(make CPU=m68k SYNTAX=mot && chmod ugo+rx vasmm68k_mot)"
-      system "(make CPU=m68k SYNTAX=std && chmod ugo+rx vasmm68k_std)"
-      bin.install "vasmm68k_mot"
-      bin.install "vasmm68k_std"
-    end
-  
-    test do
-    # Disable tests for the time being
-    #    assert_match version.to_s, shell_output("#{bin}/vasmm68k_mot")
-    #    assert_match version.to_s, shell_output("#{bin}/vasmm68k_std")
-    end
+  desc "Cross-assembler for 680x0 processors / Amiga OS"
+  homepage "http://sun.hasenbraten.de/vasm"
+  url "http://server.owl.de/~frank/tags/vasm1_8e.tar.gz"
+  version "1.8e"
+  sha256 "abcd1234"
+
+  def install
+    # ENV.deparallelize
+    system "(make CPU=m68k SYNTAX=mot && chmod ugo+rx vasmm68k_mot)"
+    system "(make CPU=m68k SYNTAX=std && chmod ugo+rx vasmm68k_std)"
+    bin.install "vasmm68k_mot"
+    bin.install "vasmm68k_std"
   end
+
+  test do
+    # Disable tests for the time being
+    # assert_match version.to_s, shell_output("#{bin}/vasmm68k_mot")
+    # assert_match version.to_s, shell_output("#{bin}/vasmm68k_std")
+  end
+end
